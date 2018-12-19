3e6a9f9b4bc8cef1144dbc05a5285f59e781cd06
Hoarfrost umber Powlowski <kaparray@gmail.com>
2018-12-19 15:10:23 +0300

    Fix absence function type_of_tetromino

--- fillit/validation.c
+++ fillit/validation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validation.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaparray <kaparray@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hpowlows <hpowlows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 16:11:52 by hpowlows          #+#    #+#             */
-/*   Updated: 2018/12/19 13:17:28 by kaparray         ###   ########.fr       */
+/*   Updated: 2018/12/19 15:09:13 by hpowlows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,24 @@ int equ_tetramino_2(int t1, int t2, int t3, int i)
 		g_lstttr.arr[i] = T19;
 	else
 		return (0);
+	return (1);
+}
+
+int type_of_tetramino(int i)
+{
+	int t1;
+	int t2;
+	int t3;
+
+	t3 = g_ttr.arr[3] - g_ttr.arr[2];
+	t2 = g_ttr.arr[2] - g_ttr.arr[1];
+	t1 = g_ttr.arr[1] - g_ttr.arr[0];
+	if (equ_tetramino_1(t1, t2, t3, i))
+		return (1);
+	else if (equ_tetramino_2(t1, t2, t3, i))
+		return (1);
+	else
+		return (0x0);
 	return (1);
 }
 
